6498e21607d1c2597dc0dca8e174c8c2374a245b
mtzern <martinszernis@gmail.com>
2021-03-02 12:23:10 +0200

    Added new/create page

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended: true})); // parses incoming requests with urlencoded payloads
+
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -27,11 +29,24 @@ app.get("/campgrounds",  async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", {campgrounds});
 });
-//=====Campground's show page, shows individual campground=====
+//=====Campgrounds new page=====
+// ordered above the show page, otherwise it wouldnt work properly
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+ });
+ //=====Campgrounds create page=====
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground); //adding to model
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`); //redirects to created campground
+ })
+//=====Campgrounds show page, shows individual campground=====
 app.get("/campgrounds/:id", async (req,res) => {
     const campground = await Campground.findById(req.params.id);  // finding corresponding campground's id
     res.render("campgrounds/show", {campground});
 });
+
+
 
 app.listen(3000, () => {
     console.log("Connected to port 3000");
